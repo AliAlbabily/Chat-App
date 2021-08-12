3972482095e7f6e60391099a8eef7a7c89866d47
DeGGi <81147016+DeGGiii@users.noreply.github.com>
2021-08-12 17:07:54 +0200

    Logs GUI klassen implementerad

---
+++ src/view/Logs.java
@@ -0,0 +1,114 @@
+package view;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Logs
+{
+    private JTextField timestamp1;
+    private JTextField timestamp2;
+    private JList<String> log;
+
+    private JButton checkBtn;
+    private JButton saveBtn;
+
+    private JFrame frame;
+    private JPanel mainPanel;
+
+    private JLabel timeStamplbl1;
+    private JLabel timeStamplbl2;
+    private Font fontlbl = new Font("Serif", Font.PLAIN, 25);
+
+    public Logs()
+    {
+        initializeComponents();
+    }
+
+    public void initializeComponents()
+    {
+        createFrame();
+        createMainPanel();
+    }
+
+    public void createFrame()
+    {
+        frame = new JFrame("Logs");
+        frame.setBounds(0, 0, 600, 650);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null); // window gets placed on the middle of the screen
+    }
+
+    public void createMainPanel()
+    {
+        Border blackline = BorderFactory.createLineBorder(Color.black);
+
+        mainPanel = new JPanel();
+        mainPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+        mainPanel.setBounds(18, 20,545, 570);
+        mainPanel.setLayout(null);
+        mainPanel.setBackground(Color.lightGray);
+
+        //Skapar timestamp lbls
+        timeStamplbl1 = new JLabel("From:");
+        timeStamplbl1.setBounds(33,10, 100, 50);
+        timeStamplbl1.setFont(fontlbl);
+        timeStamplbl1.setBackground(Color.black);
+
+        timestamp1 = new JTextField(" ~ Date ~");
+        timestamp1.setBounds(33, 60, 120, 25);
+
+        timeStamplbl2 = new JLabel("To:");
+        timeStamplbl2.setBounds(213,10, 100, 50);
+        timeStamplbl2.setFont(fontlbl);
+        timeStamplbl2.setBackground(Color.black);
+
+        timestamp2 = new JTextField(" ~ Date ~");
+        timestamp2.setBounds(213, 60, 120, 25);
+
+        checkBtn = new JButton("Check");
+        checkBtn.setBounds(392, 60, 120, 25);
+        checkBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO implementera fuktionalitet här
+            }
+        });
+
+        saveBtn = new JButton("Save");
+        saveBtn.setBounds(225, 510, 100, 50);
+        saveBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO implementera fuktionalitet här
+            }
+        });
+
+        log = new JList();
+        log.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        log.setFont(fontlbl);
+
+        JScrollPane scrollPane = new JScrollPane(log);
+        scrollPane.setBounds(33, 100, 480, 400);
+        scrollPane.getSize(log.getPreferredScrollableViewportSize());
+
+
+        mainPanel.add(timeStamplbl1);
+        mainPanel.add(timeStamplbl2);
+        mainPanel.add(timestamp1);
+        mainPanel.add(timestamp2);
+        mainPanel.add(checkBtn);
+        mainPanel.add(saveBtn);
+        mainPanel.add(scrollPane);
+        frame.add(mainPanel);
+    }
+
+//    public static void main(String[] args) {
+//        Logs logs = new Logs();
+//    }
+}
