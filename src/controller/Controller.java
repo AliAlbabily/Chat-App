7d1db287ae22b7756405c5d3c6563f362dc8748d
Mads Nörklit Johansen <31743570+Madzic8@users.noreply.github.com>
2021-08-17 11:06:56 +0200

    Kommenterat controller klassen

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -26,27 +26,27 @@ public class Controller {
 
         switch (button) {
             case Connect:
-                String username = clientGUI.getUsername();
-                ImageIcon imageIcon = clientGUI.getImageIcon();
+                String username = clientGUI.getUsername(); // Hämtar username från GUI
+                ImageIcon imageIcon = clientGUI.getImageIcon(); // Hämtar imageIcon från GUI
                 user = new User(username, imageIcon); // skapar ny User-instans
 
                 client = new Client("127.0.0.1", 2343, this); // skapar ny Client-instans
 
                 clientMainGUI = new ClientMainGUI(this); // öppna klient fönstret
-                clientGUI.closeClientConnectionWindow();
+                clientGUI.closeClientConnectionWindow(); //Stänger ner inloggningsfönster
 
-                ArrayList<User> savedContacts = contacts.fetchContactsFromFile();
+                ArrayList<User> savedContacts = contacts.fetchContactsFromFile(); //Laddar upp alla sparade kontakter från fil
                 clientMainGUI.updateContactsJList(savedContacts); // display your contacts in GUI once you are logged in
 
-                client.sendUserToServer(user);
+                client.sendUserToServer(user); //Skickar skapade usern till servern
                 break;
             case Send:
-                String textMessage = clientMainGUI.getMessage();
-                User[] selectedUsersArr = selectedUsers.toArray(new User[0]);
-                ImageIcon uploadedImage = clientMainGUI.getUploadedImage();
+                String textMessage = clientMainGUI.getMessage(); //Hämtar message från clientMain gui
+                User[] selectedUsersArr = selectedUsers.toArray(new User[0]); //Hämtar valda receivers som ska ta emot meddelande man skickar och sparar dem i array av User.
+                ImageIcon uploadedImage = clientMainGUI.getUploadedImage(); //Hämtar den valda bilden som ska skickas.
 
-                Message newMessage = new Message(user, textMessage, uploadedImage, selectedUsersArr); // create a new Message-object
-                client.sendMessageToServer(newMessage);
+                Message newMessage = new Message(user, textMessage, uploadedImage, selectedUsersArr); // Skapar ett nytt message objekt
+                client.sendMessageToServer(newMessage); //Skickar message objektet till servern
                 break;
             default:
                 System.out.println("Något gick fel!");
@@ -58,46 +58,46 @@ public class Controller {
         clientMainGUI.updateOnlineJList(onlineUsers); // updatera onlineUsers-list in client main view
     }
 
-    public void updateChatGUI(User user, String message) {
+    public void updateChatGUI(User user, String message) { //Lägger till ett nytt chattmeddelande i chatten
         clientMainGUI.updateChat(user, message);
     }
 
-    public void updateChatGUI(User user, String textMessage, ImageIcon Image) {
+    public void updateChatGUI(User user, String textMessage, ImageIcon Image) { //Lägger till ett nytt chattmeddelande i chatten
         clientMainGUI.updateChat(user, textMessage, Image);
     }
 
-    public void updateChatGUI(User user, ImageIcon Image) {
+    public void updateChatGUI(User user, ImageIcon Image) { //Lägger till ett nytt chattmeddelande i chatten
         clientMainGUI.updateChat(user, Image);
     }
 
-    public void saveNewContact(User user) {
+    public void saveNewContact(User user) { //sparar ny kontakt, sparas i fil.
         contacts.saveNewContactInFile(user);
     }
 
-    public void removeContact(User user)
+    public void removeContact(User user) //tar bort kontakt från fil
     {
         contacts.removeContactInFile(user);
     }
 
     public void addSelectedUserToReceivers(User selectedUser) {
-        selectedUsers.add(selectedUser); // save the selected user in the receivers-arraylist
+        selectedUsers.add(selectedUser); //lägg till den valda usern i vår receiver array
     }
 
     public void clearAllSelectedReceivers() {
-        selectedUsers.clear(); // remove all selected users inside the receivers-arraylist
+        selectedUsers.clear(); //Ta bort alla valda receivers
     }
 
     //<editor-fold desc="Getters and setters">
     public User getUser() {
         return user;
-    }
+    } // Hämta user
 
     public ArrayList<User> getSelectedUsers() {
         return selectedUsers;
-    }
+    } //Hämta valda users / receivers
 
     public Contacts getContacts() {
         return contacts;
-    }
+    } //Hämta kontakterna.
     //</editor-fold>
 }
